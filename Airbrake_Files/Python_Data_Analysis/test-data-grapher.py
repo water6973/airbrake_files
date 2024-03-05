77998080ee4d42d6526ed2d062ae37f1b761543f
water6973 <gavinzan@proton.me>
2024-03-04 20:23:19 -0600

    lfg

--- Airbrake_Files/Python_Data_Analysis/test-data-grapher.py
+++ Airbrake_Files/Python_Data_Analysis/test-data-grapher.py
@@ -3,19 +3,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 actual_apogee = 233  # replace with actual apogee of flight in m
+k = 0.0016
+mass = 0.641
+
+def calculate_projected_apogee(altitude, velocity):
+    return altitude + (mass / (2 * k)) * np.log((k * velocity**2) / (mass * 9.8) + 1)
 
 filename = '/home/gavin/Documents/GitHub/airbrake_files/Airbrake_Files/Flight_Data/Clean_Data/test7.csv'
 graph_filename = '/home/gavin/Documents/GitHub/airbrake_files/Airbrake_Files/Flight_Data/Graphs/test7.png'
 
 df = pd.read_csv(filename, names=['Time', 'Altitude', 'Velocity', 'isDeployed', 'isBurning', 'Projected Apogee'])
 
-df.loc[df['Time'] <= 1.5, 'Projected Apogee'] = np.nan
-df['Error'] = abs(actual_apogee - df['Projected Apogee'])
+df.loc[df['Time'] <= 1.5, 'Velocity'] = np.nan
+df['Error'] = abs(actual_apogee - calculate_projected_apogee(df['Altitude'], df['Velocity']))
+
+# Add Adjusted Velocity column
+df['Adjusted Velocity'] = df['Velocity'].copy()  # Initialize with original velocity
+for i in range(1, len(df)):
+    prev_velocity = df.at[i - 1, 'Adjusted Velocity']
+    current_velocity = df.at[i, 'Velocity']
+    if current_velocity > prev_velocity:
+        df.at[i, 'Adjusted Velocity'] = min(prev_velocity + 1, current_velocity)
+    elif current_velocity < prev_velocity:
+        df.at[i, 'Adjusted Velocity'] = max(prev_velocity - 1, current_velocity)
+
+# Calculate projected apogee using the provided equation
+df['Projected Apogee'] = calculate_projected_apogee(df['Altitude'], df['Adjusted Velocity'])
 
 # Plotting the results
 plt.figure(figsize=(10, 6))
 plt.plot(df['Time'], df['Altitude'], label='Altitude')
-plt.plot(df['Time'], df['Velocity'], label='Velocity')
+plt.plot(df['Time'], df['Adjusted Velocity'], label='Adjusted Velocity')
 plt.plot(df['Time'], df['Projected Apogee'], label='Projected Apogee')
 plt.axhline(y=actual_apogee, color='r', linestyle='--', label='Actual Apogee')
 plt.axvline(x=1500, color='r', linestyle='--', label='Motor Cutoff (1.5s)')
