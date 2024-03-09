5b282ff9f68e5ffa88ad8e8ae4cd614515a0a478
water6973 <60584141+water6973@users.noreply.github.com>
2024-03-09 14:55:43 -0600

    Update AC_DC.ino

--- Airbrake_Files/AC_DC/AC_DC.ino
+++ Airbrake_Files/AC_DC/AC_DC.ino
@@ -110,8 +110,8 @@ void setup() {
 
     Serial.println("Testing data collection...");
     myFile = SD.open("data.txt", FILE_WRITE);
-    for(int i = 0, i < 5, i++){
-      flightData[i] = 0;
+    for(int i = 0; i < 5; i++){
+      flightData[i].raw_projected_apogee = 0;
     }
     
     // if the file opened okay, write to it:
@@ -163,19 +163,19 @@ void loop() {
         // get all relevant launch variables
         flightTime = (micros()-launchTime)/1000;
         altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA)-launchAlt;
-        // velocity = (static_cast<float>(altitude - lastAltitude) / (static_cast<float>(flightTime - lastTime))) * 1000.0f; // Comment out if below debug is on
+        velocity = (static_cast<float>(altitude - lastAltitude) / (static_cast<float>(flightTime - lastTime))) * 1000.0f; // Comment out if below debug is on
 
-        // DEBUG: CODE TO TEST VELOCITY CAPPING 
+        /* DEBUG: CODE TO TEST VELOCITY CAPPING 
         if (flightTime <= 3000){
           velocity = velocity + 10;
         } else {
           velocity = velocity - 10;
         }
-        //
+        */
           
         if (flightTime <= 2000 || flightTime >= 4500){
             cappedVelocity = velocity;
-        } else {
+        } else {  
             if (velocity > cappedVelocity){
                 cappedVelocity = min(cappedVelocity + 5, velocity);
             } else if (velocity < cappedVelocity){
