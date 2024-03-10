b3c7c69a0262c1289ceacb8914dd7b5ce62e8638
water6973 <60584141+water6973@users.noreply.github.com>
2024-03-10 19:15:34 +0000

    revert

--- Airbrake_Files/AC_DC/AC_DC.ino
+++ Airbrake_Files/AC_DC/AC_DC.ino
@@ -1,15 +1,12 @@
 // The code may break if it runs for longer than 71.6 minutes due to micros() overflowing.
 // NOTE: REMEMBER TO UPDATE MASS EVERY LAUNCH!!!
-
 // MPU6050 - Version: 1.3.0
 #include <MPU6050.h>
-
 // define constants
 #define MPU6050_ACCEL_FS_16G 2
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define DEPLOYED 800
 #define UNDEPLOYED 2200
-
 // install libraries
 #include <Wire.h>
 #include <SPI.h>
@@ -18,7 +15,6 @@
 #include "Adafruit_BMP3XX.h"
 #include <I2Cdev.h>
 #include <MPU6050.h>
-
 unsigned long int launchTime;
 int flightTime; // how long in milliseconds have passed since the rocket detected a launch?
 int lastTime = 0; // in milliseconds, what was the flight time recorded in the last data entry?
@@ -28,35 +24,27 @@ float lastAltitude = 0; // in meters
 float velocity = 0; // in m/s
 float cappedVelocity = 0; // in m/s
 const int accScale = 4096;
-
 const int burnout = 1500; // in milliseconds, how long the motor will burn for
 bool isBurning = false;
 bool isLaunched = false;
-
 int numEntries = 1; // number of entries to be used in averaging projected apogee
 int entry = 0; // current entry to be updated (oldest entry)
 int meanProjectedApogee;
 
 const float mass = 0.598; // in kg, after burnout (so not including 33g of F-51 propellant)
 const float k = 0.002;
-const int accCap = 2;
 const float g = 9.8;
 const int sliceLength = 3;
 
 const bool debugMode = false;
-
 Adafruit_BMP3XX bmp;
 MPU6050 mpu;
-
 int16_t ax, ay, az; // acceleration in the x, y, and z directions
 bool isDeployed = false; // air brake deployment status
-int cappingTimer = 0;
-
 // create flight_data handler to average projected apogees
 struct FlightData {
   int raw_projected_apogee;
 };
-
 float get_projected_apogee(FlightData data[], int numEntries){
     float total = 0;
     for (int i = 0; i < numEntries; i++) {
@@ -64,10 +52,8 @@ float get_projected_apogee(FlightData data[], int numEntries){
     }
     return total / numEntries;
 }
-
 FlightData flightData[sliceLength];
 File myFile;
-
 void setup() {
     Serial.begin(115200);
     while (!Serial);
@@ -76,16 +62,14 @@ void setup() {
     Serial.println(mass, 3);
     Serial.print("K value: ");
     Serial.println(k, 4);
-    Serial.println("\n");
+    Serial.println("");
     Wire.begin();
-
     Serial.println("Initializing SD card...");
     if (!SD.begin(4)) {
         Serial.println("Initialization failed! (Hint: Put in an SD card!)");
         while (1);
     }
     Serial.println("SD card initialization successful! (1/4)\n");
-
     Serial.println("Initializing accelerometer/gyroscope...");
     mpu.initialize();
     mpu.setFullScaleAccelRange(MPU6050_ACCEL_FS_16G);
@@ -94,7 +78,6 @@ void setup() {
     digitalWrite(9,LOW);
     digitalWrite(8,LOW);
     Serial.println("Accelerometer/Gyroscope initialization successful! (2/4)\n");
-
     Serial.println("Initializing barometer...");
     if (!bmp.begin_I2C()) {
         Serial.println("Could not find a valid BMP3 sensor, check wiring!");
@@ -114,7 +97,6 @@ void setup() {
     Serial.print("Launch altitude: ");
     Serial.print(launchAlt);
     Serial.println("m\n");
-
     Serial.println("Testing data collection...");
     myFile = SD.open("data.txt", FILE_WRITE);
     for(int i = 0; i < 5; i++){
@@ -131,32 +113,22 @@ void setup() {
     myFile.println("Opened!");
     myFile.close();
     lastAltitude = bmp.readAltitude(SEALEVELPRESSURE_HPA)-launchAlt;
-
     Serial.println("Beginning loop...");
 }
-
-
-
 void loop() {
     digitalWrite(8,LOW);
-
     mpu.getAcceleration(&ax, &ay, &az); // get acceleration values from the accelerometer
-
     myFile = SD.open("data.txt", FILE_WRITE);
-
     if (! bmp.performReading()) {
         Serial.println("Barometer failed to perform reading :(");
         return;
     }
-
     altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA)-launchAlt;
-
     // detects launch by checking if acceleration is higher than 2 m/s/s and the rocket is in idle mode
     if((float)ax/accScale > 2 && !isLaunched){
         isLaunched = true;
         launchTime = micros();
     }
-
     // print out data on acceleration and launch status
     Serial.print("Flight Time (ms): ");
     Serial.println(flightTime);
@@ -164,14 +136,12 @@ void loop() {
     Serial.println((float)ax/accScale);
     Serial.print("Launch status: ");
     Serial.println(isLaunched);
-
     // if launch is detected and rocket has been flying for less than 20 seconds
     if((isLaunched && flightTime <= 20000) or debugMode){
         // get all relevant launch variables
         flightTime = (micros()-launchTime)/1000;
         altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA)-launchAlt;
         velocity = (static_cast<float>(altitude - lastAltitude) / (static_cast<float>(flightTime - lastTime))) * 1000.0f; // Comment out if below debug is on
-
         /* DEBUG: CODE TO TEST VELOCITY CAPPING 
         if (flightTime <= 3000){
           velocity = velocity + 10;
@@ -179,63 +149,41 @@ void loop() {
           velocity = velocity - 10;
         }
         */
-          
-        if (cappingTimer > 0){
-            cappingTimer -= 1;
-            if (velocity > cappedVelocity){
-                cappedVelocity = min(cappedVelocity + accCap, velocity);
-            } else if (velocity < cappedVelocity){
-                cappedVelocity = max(cappedVelocity - accCap, velocity);
-            }
-        } else {  
-            cappedVelocity = velocity;
-        }
 
+        if (flightTime <= 2000 || flightTime >= 5000){
+            cappedVelocity = velocity;
+        } else {  
+            if (velocity > cappedVelocity){
+                cappedVelocity = min(cappedVelocity + 2, velocity);
+            } else if (velocity < cappedVelocity){
+                cappedVelocity = max(cappedVelocity - 2, velocity);
+            }
+        }
         lastTime = flightTime;
         lastAltitude = altitude;
         isBurning = flightTime <= burnout;
         flightData[entry].raw_projected_apogee = altitude + (mass / (2*k)) * log((k * pow(cappedVelocity, 2)) / (mass * 9.8) + 1);
-
         // update code
         meanProjectedApogee = get_projected_apogee(flightData, numEntries);
         if (meanProjectedApogee > 250){
-            if (!isDeployed){
-                cappingTimer = 5;
-            }
             isDeployed = true;
-        } else { 
-            if (isDeployed){
-                cappingTimer = 5;
-            }
-            isDeployed = false; 
-        }
-
+        } else { isDeployed = false; }
         /* DEBUG: CODE TO TEST AIRBRAKE DEPLOYMENT
         isDeployed = true;
         */
-
         // record out relevant launch variables to the data file (data.txt)
         myFile.print(flightTime);
         myFile.print(", ");
-
         myFile.print(altitude);
         myFile.print(", ");
-
         myFile.print(cappedVelocity);
         myFile.print(", ");
-
-        myFile.print(cappingTimer);
-        myFile.print(", ");
-
         myFile.print(isDeployed);
         myFile.print(", ");
-
         myFile.print(isBurning);
         myFile.print(", ");
-
         myFile.print(meanProjectedApogee);
         myFile.println();
-
         /* DEBUG: CODE TO TEST APOGEE AVERAGING
         Serial.print(meanProjectedApogee);
         Serial.print(" ");
@@ -249,11 +197,9 @@ void loop() {
         Serial.print(" ");
         Serial.println(flightData[4].raw_projected_apogee);
         */
-
         entry = (entry + 1) % sliceLength;
         numEntries = min(numEntries + 1, sliceLength);
     }
-
     // if rocket has been flying for more than two seconds
     if (flightTime >= 2000 && flightTime < 10000){
       if (isDeployed){
@@ -267,7 +213,6 @@ void loop() {
         digitalWrite(9,LOW);
       }
     }
-
     // if rocket has been flying for more than 10 seconds
     if (flightTime >= 10000){
         isDeployed = false;
@@ -275,14 +220,12 @@ void loop() {
         delayMicroseconds(UNDEPLOYED);
         digitalWrite(9,LOW);
     }
-
     // if the rocket has been flying for more than 20 seconds
     if (flightTime >= 20000){
         // set rocket to idle
         isLaunched = false;
         flightTime = 0; // reset flight time
     }
-
     // close data file to save changes
     myFile.close();
 }
