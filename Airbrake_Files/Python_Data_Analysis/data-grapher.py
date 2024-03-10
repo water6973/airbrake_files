8951be31ac882e44e7a25c8aaed73af4ce771655
water6973 <gavinzan@proton.me>
2024-03-10 10:04:44 -0500

    graphed data

--- Airbrake_Files/Python_Data_Analysis/data-grapher.py
+++ Airbrake_Files/Python_Data_Analysis/data-grapher.py
@@ -2,30 +2,39 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+PNUT = 0
+SD = 1
+
 actual_apogee = 233  # replace with actual apogee of flight in m
 
-filename = '/home/gavin/Documents/GitHub/airbrake_files/Airbrake_Files/Flight_Data/Clean_Data/test7.csv'
-graph_filename = '/home/gavin/Documents/GitHub/airbrake_files/Airbrake_Files/Flight_Data/Graphs/test7.png'
+datatype = SD
+filename = '/home/gavin/Documents/GitHub/airbrake_files/Airbrake_Files/Flight_Data/Raw_Data/test8_raw.csv'
+graph_filename = '/home/gavin/Documents/GitHub/airbrake_files/Airbrake_Files/Flight_Data/Graphs/test8.png'
 
-df = pd.read_csv(filename, names=['Time', 'Altitude', 'Velocity', 'isDeployed', 'isBurning', 'Projected Apogee'])
+if (datatype == SD):
+    df = pd.read_csv(filename, names=['Time', 'Altitude', 'Velocity', 'isDeployed', 'isBurning', 'Projected Apogee'])
+    df.loc[df['Time'] <= 1.5, 'Projected Apogee'] = np.nan
+    df['Error'] = abs(actual_apogee - df['Projected Apogee'])
 
-df.loc[df['Time'] <= 1.5, 'Projected Apogee'] = np.nan
-df['Error'] = abs(actual_apogee - df['Projected Apogee'])
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['Time'], df['Altitude'], label='Altitude')
+    plt.plot(df['Time'], df['Velocity'], label='Velocity')
+    plt.plot(df['Time'], df['Projected Apogee'], label='Projected Apogee')
+    plt.axhline(y=actual_apogee, color='r', linestyle='--', label='Actual Apogee')
+    plt.axvline(x=1.5, color='r', linestyle='--', label='Motor Cutoff (1.5s)')
 
-# Plotting the results
-plt.figure(figsize=(10, 6))
-plt.plot(df['Time'], df['Altitude'], label='Altitude')
-plt.plot(df['Time'], df['Velocity'], label='Velocity')
-plt.plot(df['Time'], df['Projected Apogee'], label='Projected Apogee')
-plt.axhline(y=actual_apogee, color='r', linestyle='--', label='Actual Apogee')
-plt.axvline(x=1500, color='r', linestyle='--', label='Motor Cutoff (1.5s)')
+    deploy_changes = df[df['isDeployed'].diff() != 0]
+    for time in deploy_changes['Time']:
+        plt.axvline(x=time, color='k', linestyle='--', alpha=0.5)
+elif (datatype == PNUT):
+    df = pd.read_csv(filename, names=['Time', 'Altitude', 'Velocity', 'Temperature', 'Voltage'])
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['Time'], df['Altitude'], label='Altitude')
+    plt.plot(df['Time'], df['Velocity'], label='Velocity')
+    plt.axvline(x=1.5, color='r', linestyle='--', label='Motor Cutoff (1.5s)')
 
-deploy_changes = df[df['isDeployed'].diff() != 0]
-for time in deploy_changes['Time']:
-    plt.axvline(x=time, color='k', linestyle='--', alpha=0.5)
-
-plt.ylim(0, 400)
-plt.xlim(0, 10000)
+plt.ylim(0, 1000)
+plt.xlim(0, 60)
 
 plt.xlabel('Time (Milliseconds)')
 plt.ylabel('Altitude (Meters)')
